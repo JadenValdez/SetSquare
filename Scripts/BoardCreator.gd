ddf47c45396c0ac19b4972f6c3af19ef1dadfa7b
Ghost-256 <jadenvaldez02@gmail.com>
2026-07-21 23:57:57 -0700

    Added Two Tile Lists
    Added 2 tiles lists, one with 3 attributes and one with 4.

--- Scripts/BoardCreator.gd
+++ Scripts/BoardCreator.gd
@@ -12,14 +12,27 @@ const SHAPES: Dictionary = {
 	"Triangle": null
 }
 
+const FILL: Dictionary = {
+	"Solid": null,
+	"Shaded": null,
+	"Empty": null
+}
+
 var tile_list: Array
+var large_tile_list: Array
+#maybe an "extra" list with 5?
+
+var chosen_tiles: Array
 var rng = RandomNumberGenerator.new()
 var index: int
 
 func _ready() -> void:
-	create_tile_list()
+	create_tile_lists()
+	create_board()
 
-func create_tile_list() -> void:
+func create_tile_lists() -> void:
+	tile_list = []
+	large_tile_list = []
 	for i in COLORS:
 		for j in SHAPES:
 			for k in range(3):
@@ -27,9 +40,23 @@ func create_tile_list() -> void:
 					"Color": i,
 					"Shape": j,
 					"Amount": k+1
-				})
+					})
+				for l in FILL:
+					large_tile_list.append({
+						"Color": i,
+						"Shape": j,
+						"Fill": l,
+						"Amount": k+1
+					})
 
 func create_board() -> void:
+	chosen_tiles = []
 	for i in range(3):
 		for j in range(3):
-			index = rng.randi(tile_list.size())
+			index = rng.randi_range(0, tile_list.size() - 1)
+			chosen_tiles.append(tile_list[index])
+			tile_list.remove_at(index)
+			check_answers()
+
+func check_answers() -> void:
+	pass
