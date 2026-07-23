4cdd963e4a0ca9b6d2296bbcd16a1cf8dc23f2d5
Ghost-256 <jadenvaldez02@gmail.com>
2026-07-22 23:59:56 -0700

    Reworked Tile Saving
    Saved tiles as IDs the explains its attributes, rather than just dictionaries.

--- Scripts/BoardCreator.gd
+++ Scripts/BoardCreator.gd
@@ -1,62 +1,70 @@
 extends Node2D
 
 const COLORS: Dictionary = {
-	"Red": Color(1, 0, 0, 1),
-	"Yellow": Color(1, 1, 0, 1),
-	"Blue": Color(0, 0, 1, 1)
+	0: "Red",
+	1: "Yellow",
+	2: "Blue"
 }
 
 const SHAPES: Dictionary = {
-	"Circle": null,
-	"Square": null,
-	"Triangle": null
+	0: "Circle",
+	1: "Square",
+	2: "Triangle"
 }
 
 const FILL: Dictionary = {
-	"Solid": null,
-	"Shaded": null,
-	"Empty": null
+	0: "Solid",
+	1: "Shaded",
+	2: "Empty"
 }
 
-var tile_list: Array
-var large_tile_list: Array
+var tile_list: Dictionary
+var large_tile_list: Dictionary
 #maybe an "extra" list with 5?
 
+var size: int
 var chosen_tiles: Array
-var rng = RandomNumberGenerator.new()
-var index: int
+var answer_tile: String
+var answers: Dictionary
+var index: String
 
 func _ready() -> void:
 	create_tile_lists()
-	create_board()
+	create_board(2, 3)
 
 func create_tile_lists() -> void:
-	tile_list = []
-	large_tile_list = []
-	for i in COLORS:
-		for j in SHAPES:
-			for k in range(3):
-				tile_list.append({
-					"Color": i,
-					"Shape": j,
-					"Amount": k+1
-					})
-				for l in FILL:
-					large_tile_list.append({
-						"Color": i,
-						"Shape": j,
-						"Fill": l,
-						"Amount": k+1
-					})
-
-func create_board() -> void:
-	chosen_tiles = []
+	tile_list = {}
+	large_tile_list = {}
 	for i in range(3):
 		for j in range(3):
-			index = rng.randi_range(0, tile_list.size() - 1)
-			chosen_tiles.append(tile_list[index])
-			tile_list.remove_at(index)
-			check_answers()
+			for k in range(3):
+				
+				tile_list[str(i) + str(j) + str(k)] = {
+					"Color": COLORS[i],
+					"Shape": SHAPES[j],
+					"Amount": k+1
+					}
+					
+				for l in FILL:
+					large_tile_list[str(i) + str(j) + str(l) + str(k)] = {
+						"Color": COLORS[i],
+						"Shape": SHAPES[j],
+						"Fill": FILL[l],
+						"Amount": k+1
+					}
 
-func check_answers() -> void:
-	pass
+func create_board(rows: int, columns: int) -> void:
+	chosen_tiles = []
+	answers = {}
+	for i in range(rows):
+		for j in range(columns):
+			size = tile_list.size()
+			index = tile_list.keys()[randi() % size]
+			get_answers(tile_list[index])
+
+func get_answers(new_tile: String) -> void:
+	for x in chosen_tiles:
+		answer_tile = ""
+		if x[0] == new_tile[0]:
+			answer_tile += x[0]
+	chosen_tiles.append(new_tile)
