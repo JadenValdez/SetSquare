417052e92d424125196380cc9c653219adf908e9
Ghost-256 <jadenvaldez02@gmail.com>
2026-07-23 23:50:56 -0700

    Added Answer Finder
    Added code that finds the answers to the currently chosen tiles, which will be used to make sure each board has a limited amount of answers.

--- Scripts/BoardCreator.gd
+++ Scripts/BoardCreator.gd
@@ -23,7 +23,7 @@ var large_tile_list: Dictionary
 #maybe an "extra" list with 5?
 
 var size: int
-var chosen_tiles: Array
+var chosen_tiles: Dictionary
 var answer_tile: String
 var answers: Dictionary
 var index: String
@@ -54,7 +54,7 @@ func create_tile_lists() -> void:
 					}
 
 func create_board(rows: int, columns: int) -> void:
-	chosen_tiles = []
+	chosen_tiles = {}
 	answers = {}
 	for i in range(rows):
 		for j in range(columns):
@@ -63,8 +63,29 @@ func create_board(rows: int, columns: int) -> void:
 			get_answers(tile_list[index])
 
 func get_answers(new_tile: String) -> void:
-	for x in chosen_tiles:
+	for x in chosen_tiles.keys():
 		answer_tile = ""
-		if x[0] == new_tile[0]:
-			answer_tile += x[0]
-	chosen_tiles.append(new_tile)
+		
+		#use range 4 for larger list
+		for i in range(3):
+			if x[i] == new_tile[i]:
+				answer_tile += x[i]
+			else: 
+				match [x[i], new_tile[i]]:
+					["0", "1"]:
+						answer_tile += "2"
+					["1", "0"]:
+						answer_tile += "2"
+					["0", "2"]:
+						answer_tile += "1"
+					["2", "0"]:
+						answer_tile += "1"
+					_:
+						answer_tile += "0"
+					
+	tile_list.erase(answer_tile)
+	
+	if !chosen_tiles.has(answer_tile):
+		chosen_tiles[answer_tile] = 1
+	else:
+		chosen_tiles[answer_tile] += 1
